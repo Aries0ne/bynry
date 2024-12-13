4e2dc23edc6ef0edf4f1931502d112cfe96cae14
Naman Singh Bhati <102287344+Aries0ne@users.noreply.github.com>
2024-12-13 09:37:47 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProfileListComponent },
+  { path: 'admin', component: AdminPanelComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
